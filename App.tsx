595201efc360752a61660cea4366f583280a4dfd
Samy Senani <samy.senani@deepor.ai>
2022-05-15 13:38:28 +0200

    add: react query --> app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { NativeBaseProvider } from "native-base";
 import React from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
 import theme from "./theme";
 import Routing from "./routing";
+
+// Create a client
+const queryClient = new QueryClient();
 
 export default function App() {
   return (
     <NativeBaseProvider theme={theme}>
-      <Routing />
+      <QueryClientProvider client={queryClient}>
+        <Routing />
+      </QueryClientProvider>
     </NativeBaseProvider>
   );
 }
